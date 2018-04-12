638ad8101729f712a82f3cab046071404b253284
Ivan Jobs <ivan1377@163.com>
2018-04-12 11:31:41 +0800

    use cmake to compile

--- binary_search_tree.hpp
+++ binary_search_tree.hpp
@@ -1,3 +1,22 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
+struct BSTNode {
+  int key_;
+  int associated_value_;
+};
+
+
+class BinarySearchTree {
+  public:
+    void Insert(int key, int associated_value); // add new node by (key, associated_value).
+    BSTNode * Search(int key); // search node by key.
+    void Delete(int key); // delete node by key.
+    bool Check(); // verification.
+    void Travel(); // in order traversal.
+    void Show(); // showing bst in ascii style graph.
+  private:
+    BSTNode * root{nullptr};
+};
 
--- binary_search_tree_test.cpp
+++ binary_search_tree_test.cpp
@@ -1,6 +1,29 @@
 #include "binary_search_tree.hpp"
 
+using namespace std;
+
 int main() {
+  BinarySearchTree bst;
+  for (int i = 1; i < 10; i++) {
+    bst.Insert(i, i);
+  }
+
+  bst.Show();
+
+  if (bst.Check()) {
+    cout<<"Pass case 1"<<endl;
+  } else {
+    cout<<"Fail case 1"<<endl;
+  }
+
+  bst.Delete(5);
+  bst.Show();
+
+  if (bst.Check()) {
+    cout<<"Pass case 2"<<endl;
+  } else {
+    cout<<"Fail case 2"<<endl;
+  }
 
   return 0;
 }
