f8894b8bd5a38b5ef975a8e895ea7b3848d5a570
Ivan Jobs <ivan1377@163.com>
2018-04-18 15:07:20 +0800

    wip

--- trie.hpp
+++ trie.hpp
@@ -2,4 +2,19 @@
 
 using namespace std;
 
-class Trie {};
+struct TrieNode {
+  TrieNode() {
+    children_.clear();
+    children_.resize(26, nullptr); 
+  }
+  
+  bool leaf_{true}; 
+  vector<struct TrieNode *> children_; 
+};
+
+class StringTrie {
+  public:
+    // insert a string
+    void Insert(const string & s);
+    bool Search(const string & s); 
+};
