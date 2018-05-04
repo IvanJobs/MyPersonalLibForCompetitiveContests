cd26ba2c2ea4475e2529b8fd58c5573634baf8ae
Ivan Jobs <ivan1377@163.com>
2018-05-05 01:28:24 +0800

    trie wip

--- trie.hpp
+++ trie.hpp
@@ -1,20 +1,59 @@
+#ifndef TRIE_HPP_
+#define TRIE_HPP_
 #include <bits/stdc++.h>
 
 using namespace std;
+#define BRANCH_NUM 26
+struct DummyNode {
+  DummyNode() {
+    for (int i = 0; i < BRANCH_NUM; i++) {
+      children_[i] = nullptr;
+    }
 
-struct TrieNode {
-  TrieNode() {
-    children_.clear();
-    children_.resize(26, nullptr); 
   }
-  
-  bool leaf_{true}; 
-  vector<struct TrieNode *> children_; 
+  char c_{'A'};
+  bool final_{false};
+  struct DummyNode * children_[BRANCH_NUM]; 
 };
-
-class StringTrie {
+using TrieNode = struct DummyNode;
+class Trie {
   public:
+    Trie() {
+      flat_nodes_.push_back(TrieNode());
+      root_ = &flat_nodes_[flat_nodes.size() - 1];
+    }
     // insert a string
-    void Insert(const string & s);
-    bool Search(const string & s); 
+    void Insert(const string & s) {
+      if (s == "") return ;
+
+      TrieNode * curr = root_;
+      for (int i = 0; i < s.size(); i++) {
+        char c = s[i];
+        int idx = c - 'a';
+        TrieNode * new_node = nullptr;
+        
+        if (curr->children_[idx] == nullptr) {
+          new_node = newTrieNode();
+          new_node->c_ = c;
+          curr->children_[idx] = new_node;
+        }
+
+        if (i == s.size() - 1) {
+          new_node->final_ = true;
+        }
+        curr = new_node;
+      }  
+    }
+    bool Has(const string & s) {
+       
+    } 
+  private:
+    TrieNode * newTrieNode() {
+      flat_nodes_.push_back(TrieNode());
+      return &flat_nodes_[flat_nodes_.size() - 1];
+    }
+  private:
+    TrieNode * root_{nullptr};
+    vector<TrieNode> flat_nodes_;
 };
+#endif
