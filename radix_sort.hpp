33ee9b9cfc4923689ae3295bae936f73a0e32f04
IvanJobs <ivan1377@163.com>
2018-05-25 15:34:00 +0800

    add radix sort

---
+++ radix_sort.hpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class RadixSort {
+    public:
+        static void Sort(std::vector<int> & a) {
+            // get max position count.
+            // Example: IntWidth(12345) == 5
+            auto IntWidth = [](int v) {
+                int res = 0;
+                do {
+                    res++;
+                    v /= 10;
+                } while(v > 0);
+                return res;
+            };
+
+            // Example: GetShiftValue(12345, 2) == 4
+            auto GetShiftValue = [](int v, int idx) {
+                int res = v % 10;
+                do {
+                   idx--;
+                   if (idx <= 0) break;
+                   v /= 10;
+                   res = v % 10;
+                } while(true);
+
+                return res;
+            };
+
+            int max_width = 0;
+            for (auto & v: a) {
+                max_width = max(max_width, IntWidth(v));
+            }
+
+            // becasue of 0..9
+            int buckets[10];
+            std::vector<int> b;
+            b.resize(a.size());
+            for (int i = 1; i <= max_width; i++) {
+                memset(buckets, 0, sizeof(buckets));
+                std::vector<int> digits_cache;
+                for (size_t j = 0; j < a.size(); j++) {
+                    int d = GetShiftValue(a[j], i);
+                    digits_cache.push_back(d);
+                    buckets[d]++;
+                } 
+
+                // accum sum of buckets.
+                for (int j = 1; j < 10; j++) buckets[j] += buckets[j - 1];
+
+                for (size_t j = a.size() - 1; j >= 0; j--) {
+                    int d = digits_cache[j];
+                    b[buckets[d] - 1] = a[j];
+                    buckets[d]--;
+                }
+
+                // swap a with b
+                for (size_t j = 0; j < a.size(); j++) {
+                    swap(a[j], b[j]);
+                }
+            }
+        }
+};
--- radix_sort_test.cpp
+++ radix_sort_test.cpp
@@ -1,21 +1,20 @@
 #include <bits/stdc++.h>
 #include <gtest/gtest.h>
+#include "radix_sort.hpp"
 
 using namespace std;
-
-void RadixSort(vector<int> & v) {
-    sort(v.begin(), v.end());
-}
 
 TEST(RadixSort, RadixSort) {
     vector<int> v {11, 22, 34, 9, 21, 18, 1000, 550};
     vector<int> r {9, 11, 18, 21, 22, 34, 550, 1000};
-    RadixSort(v);
+    RadixSort::Sort(v);
     for (size_t i = 0; i < v.size(); i++) {
         EXPECT_TRUE(v[i] == r[i]);
     }
 }
+
 int main(int argc, char * argv[]) {
     testing::InitGoogleTest(&argc, argv);
-    return 0;
+
+    return RUN_ALL_TESTS();
 }
