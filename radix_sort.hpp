e029d11c9a38ec77d696a32be6c77e41da7584a4
IvanJobs <ivan1377@163.com>
2018-05-28 16:57:10 +0800

    size_t i, i-- type overflowgit statusgit status!

--- radix_sort.hpp
+++ radix_sort.hpp
@@ -4,31 +4,28 @@ using namespace std;
 
 class RadixSort {
     public:
+        static int IntWidth(int v) {
+            int res = 0;
+            do {
+                res++;
+                v /= 10;
+            } while(v > 0);
+            return res;
+        }
+
+        static int GetShiftValue(int v, int idx) {
+            int res = v % 10;
+            do {
+               idx--;
+               if (idx <= 0) break;
+               v /= 10;
+               res = v % 10;
+            } while(true);
+
+            return res;
+        }
+
         static void Sort(std::vector<int> & a) {
-            // get max position count.
-            // Example: IntWidth(12345) == 5
-            auto IntWidth = [](int v) {
-                int res = 0;
-                do {
-                    res++;
-                    v /= 10;
-                } while(v > 0);
-                return res;
-            };
-
-            // Example: GetShiftValue(12345, 2) == 4
-            auto GetShiftValue = [](int v, int idx) {
-                int res = v % 10;
-                do {
-                   idx--;
-                   if (idx <= 0) break;
-                   v /= 10;
-                   res = v % 10;
-                } while(true);
-
-                return res;
-            };
-
             int max_width = 0;
             for (auto & v: a) {
                 max_width = max(max_width, IntWidth(v));
@@ -39,19 +36,19 @@ class RadixSort {
             std::vector<int> b;
             b.resize(a.size());
             for (int i = 1; i <= max_width; i++) {
-                memset(buckets, 0, sizeof(buckets));
+                for (size_t j = 0; j < 10; j++) buckets[j] = 0;
                 std::vector<int> digits_cache;
+                digits_cache.clear();
                 for (size_t j = 0; j < a.size(); j++) {
                     int d = GetShiftValue(a[j], i);
-                    cout<<d<<endl;
                     digits_cache.push_back(d);
                     buckets[d]++;
                 } 
 
                 // accum sum of buckets.
                 for (int j = 1; j < 10; j++) buckets[j] += buckets[j - 1];
-
-                for (size_t j = a.size() - 1; j >= 0; j--) {
+                
+                for (int j = a.size() - 1; j >= 0; j--) {
                     int d = digits_cache[j];
                     b[buckets[d] - 1] = a[j];
                     buckets[d]--;
@@ -59,7 +56,7 @@ class RadixSort {
 
                 // swap a with b
                 for (size_t j = 0; j < a.size(); j++) {
-                    swap(a[j], b[j]);
+                    a[j] = b[j];
                 }
             }
         }
--- radix_sort_test.cpp
+++ radix_sort_test.cpp
@@ -4,6 +4,22 @@
 
 using namespace std;
 
+TEST(RadixSort, Pre) {
+    EXPECT_TRUE(RadixSort::IntWidth(0) == 1);
+    EXPECT_TRUE(RadixSort::IntWidth(12) == 2);
+    EXPECT_TRUE(RadixSort::IntWidth(123) == 3);
+    EXPECT_TRUE(RadixSort::IntWidth(1234) == 4);
+    EXPECT_TRUE(RadixSort::IntWidth(12345) == 5);
+    EXPECT_TRUE(RadixSort::IntWidth(123456) == 6);
+
+    EXPECT_TRUE(RadixSort::GetShiftValue(123, 1) == 3);
+    EXPECT_TRUE(RadixSort::GetShiftValue(123, 2) == 2);
+    EXPECT_TRUE(RadixSort::GetShiftValue(123, 3) == 1);
+    EXPECT_TRUE(RadixSort::GetShiftValue(123, 4) == 0);
+    EXPECT_TRUE(RadixSort::GetShiftValue(123, 5) == 0);
+    EXPECT_TRUE(RadixSort::GetShiftValue(123, 6) == 0);
+}
+
 TEST(RadixSort, RadixSort) {
     vector<int> v {11, 22, 34, 9, 21, 18, 1000, 550};
     vector<int> r {9, 11, 18, 21, 22, 34, 550, 1000};
